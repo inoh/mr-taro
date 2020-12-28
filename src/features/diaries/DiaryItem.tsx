144ffae142d96a519cc598031d0031f0b52094d6
ino_h <syslink.h.inoue@gmail.com>
2020-12-28 15:22:51 +0900

    Change speak design

--- src/features/diaries/DiaryItem.tsx
+++ src/features/diaries/DiaryItem.tsx
@@ -1,54 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+import { Microphone } from '@styled-icons/boxicons-solid'
 
 import { RootState } from 'app/rootReducer'
-import Button from 'components/Button'
 import TimeLabel from 'components/TimeLabel'
 import { Diary } from './diariesSlice'
 import { getDiaryAudioBuffer } from 'apis/diary'
 
-const Item = styled.li<{ isLatest: boolean }>`
+const Item = styled.li<{ opened: boolean }>`
   padding: 10px;
   border-bottom: 1px solid rgba(38, 0, 51, 1);
   background-color: #fff;
-  ${({ isLatest }) => isLatest && "box-shadow: inset 0 10px 25px 0 rgba(38, 0, 51, 0.1);"}
-`;
+  ${({ opened }) => opened && "box-shadow: inset 0 10px 25px 0 rgba(38, 0, 51, 0.1);"}
+`
 
 const Note = styled.p`
   margin: 0;
-  padding: 5px;
+  padding: 10px 10px 0 10px;
   font-size: 15px;
-`;
+`
+
+const StyledMicrophone = styled(Microphone)`
+  cursor: pointer;
+  width: 16px;
+  margin-right: 5px;
+  vertical-align: bottom;
+
+  :hover {
+    opacity: 0.3;
+  }
+`
 
 type Props = Diary
 
 export const DiaryItem = ({ id, pages }: Props) => {
-  const [opened, setOpened] = useState(false);
   const {
     latestAddedDiaryId,
   } = useSelector((state: RootState) => state.diaries)
+  const [opened, setOpened] = useState(!!latestAddedDiaryId);
 
-  const speech = async (diaryId: string, lang: string) => {
+  useEffect(() => {
+    if (id === latestAddedDiaryId) {
+      speech(latestAddedDiaryId)
+    }
+  }, [latestAddedDiaryId])
+
+  const speech = async (diaryId: string) => {
     const context = new AudioContext()
     const source = context.createBufferSource()
-    source.buffer = await getDiaryAudioBuffer(context, diaryId, lang)
+    source.buffer = await getDiaryAudioBuffer(context, diaryId, 'En')
     source.connect(context.destination)
     source.start()
   }
 
   const jaOpenAndClose = () => {
-    setOpened(!opened);
+    setOpened(!opened)
   }
 
   return (
-    <Item isLatest={id === latestAddedDiaryId}>
+    <Item opened={opened}>
+      <StyledMicrophone onClick={() => speech(id)} />
       <TimeLabel date={new Date(pages.En.posted_at)} />
 
       <Note onClick={jaOpenAndClose}>{pages.En.note}</Note>
       {opened && <Note>{pages.Ja.note}</Note>}
-
-      <Button onClick={() => speech(id, 'En')}>En</Button>
     </Item>
-  );
+  )
 }
